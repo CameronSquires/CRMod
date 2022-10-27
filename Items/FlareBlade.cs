cc23a77815ce36ef05ca67c03b567b209a258567
CameronSquires <87430045+CameronSquires@users.noreply.github.com>
2022-10-26 21:31:26 -0230

    Added Flare Blade.

---
+++ Items/FlareBlade.cs
@@ -0,0 +1,52 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CRMod.Items
+{
+    public class FlareBlade : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Flare Blade"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+            Tooltip.SetDefault("Control the power of the flares.");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 24;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = 1;
+            Item.knockBack = 6;
+            Item.value = Item.sellPrice(silver: 30);
+            Item.rare = 2;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+        }
+
+
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(BuffID.OnFire, 120);
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Flare, 10);
+            recipe.AddIngredient(ItemID.IronBar, 8);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+
+            Recipe recipe2 = CreateRecipe();
+            recipe2.AddIngredient(ItemID.Flare, 10);
+            recipe2.AddIngredient(ItemID.LeadBar, 8);
+            recipe2.AddTile(TileID.Anvils);
+            recipe2.Register();
+        }
+    }
+}
